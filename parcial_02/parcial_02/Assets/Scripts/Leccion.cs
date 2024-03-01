7cad411e2f612d5cdb347aa7af23055fb3e7b707
Jaime Ramírez Vázquez <jaime.ramirez@uteca.edu.mx>
2024-03-01 00:34:01 -0600

    Main - add subject and leccion script

---
+++ parcial_02/parcial_02/Assets/Scripts/Leccion.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leccion
+{
+    public int ID;
+    public string lessons;
+    public List<string> options;
+    public int correctAnswer;
+}
---
+++ parcial_02/parcial_02/Assets/Scripts/Subject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Leccion
+{
+    public int ID;
+    public List<string> lessons;
+    public List<string> potions;
+    public int correctAnswer;
+}
+
+[CreateAssetMenu(fileName = "New Subject", menuName = "ScriptableObjects/NewLesson", order = 1)]
+
+public class Subject : ScriptableObject
+{
+    public List<Leccion> leccionList;
+}
+    
