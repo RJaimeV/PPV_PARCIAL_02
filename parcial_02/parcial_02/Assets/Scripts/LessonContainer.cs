9c06cc4b2a2c60a7aaa287cb374a1b17b3f9c1ae
Jaime Ramírez Vázquez <jaime.ramirez@uteca.edu.mx>
2024-03-09 18:33:38 -0600

    Main - create lessoncontainer script

--- parcial_02/parcial_02/Assets/Scripts/LessonContainer.cs
+++ parcial_02/parcial_02/Assets/Scripts/LessonContainer.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LessonContainer : MonoBehaviour
 {
     [Header("GamePbject Configuration")]
-    public int Lection = 0;
+    public int Lesson = 0;
     public int CurrentLesson = 0;
     public int TotalLessons = 0;
     public bool AreAllLessonsComplete = false;
-
+    
     [Header("UI Configuration")]  
+
     public TMP_Text StageTitle;
     public TMP_Text LessonStage;
 
     [Header("External GameObject Configuration")]
     public GameObject lessonContainer;
+
+    [Header("Lesson Data")]
+    public GameObject LessonData;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,7 +39,7 @@ public class LessonContainer : MonoBehaviour
     {
         if (StageTitle != null || LessonStage != null )
         {
-            StageTitle.text = "Leccion " + Lection;
+            StageTitle.text = "Leccion " + Lesson;
             LessonStage.text = "Leccion " + CurrentLesson + " de " + TotalLessons;
         }
         else
