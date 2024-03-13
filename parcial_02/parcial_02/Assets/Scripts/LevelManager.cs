8fa1b67b137cae124f700bf0a1f9bfe2eec67b04
Jaime Ramírez Vázquez <jaime.ramirez@uteca.edu.mx>
2024-03-13 14:14:01 -0600

    MAIN - ADD BOTTONS

--- parcial_02/parcial_02/Assets/Scripts/LevelManager.cs
+++ parcial_02/parcial_02/Assets/Scripts/LevelManager.cs
@@ -1,26 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
+    public static LevelManager Instance;
+
     [Header("Level Data")]
-    public ScriptableObject Lesson;
+    public Subject Lesson;
+
+    [Header("Use Interface")]
+    public TMP_Text QuestionTxt;
+    public List<Option> Options;
 
     [Header("Game Configuration")]
     public int questionAmount = 0;
     public int currentQuestion = 0;
-    public string Question;
+    public string question;
     public string correctAnswer;
+    public int answerFromPlayer;
+
+    [Header("Current lesson")]
+    public Leccion currentLesson;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            return;
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        questionAmount = Lesson.leccionlist.Count;
+        //Establecemos la cantidad de preguntas en la lección
+        questionAmount = Lesson.leccionList.Count;
+        //Cargar la primera pregunta
+        LoadQuestion();
     }
 
     // Update is called once per frame
-    void Update()
+    private void LoadQuestion()
     {
-        
+        //Aseguramos que la pregunta actual este dentro de los límites
+        if(currentQuestion < questionAmount)
+        {
+            //Establecemos la lección actual
+            currentLesson = Lesson.leccionList[currentQuestion];
+            //Establecemos la pregunta
+            question = currentLesson.lessons;
+            //Establecemos la respuesta correcta
+            correctAnswer = currentLesson.options[currentLesson.correctAnswer];
+            //Establecemos la pregunta en UI
+            QuestionTxt.text = question;
+           for (int i = 0; i < currentLesson.options.Count; i++)
+            {
+                Options[i].GetComponent<Option>().OptionName = currentLesson.options[i];
+                Options[i].GetComponent<Option>().OptionID = i;
+                Options[i].GetComponent<Option>().UpdateText();
+            }
+        }
+        else
+        {
+            //Si llegamos al final de las preguntas
+            Debug.Log("Fin de las preguntas");
+        }
+    }
+    public void NextQuestion()
+    {
+        if (currentQuestion < questionAmount)
+        {
+            // Incrementamos el indice de la pregunta actual
+            currentQuestion++;
+            //Cargar la nueva pregunta
+            LoadQuestion();
+        }
+        else
+        {
+            //Cambio de escena
+        }
+    }
+
+    public void SetPlayerAnswer(int _answer)
+    {
+        answerFromPlayer = _answer;
     }
 }
---
+++ parcial_02/parcial_02/Assets/Scripts/Option.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Option : MonoBehaviour
+{
+    public int OptionID;
+    public string OptionName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetChild(0).GetComponent<TMP_Text>().text = OptionName;
+    }
+
+    public void UpdateText()
+    {
+        transform.GetChild(0).GetComponent<TMP_Text>().text = OptionName;
+    }
+
+    public void SelectOption()
+    {
+        LevelManager.Instance.SetPlayerAnswer(OptionID);
+    }
+}
